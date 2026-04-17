3921b1af1e6f7ba536cf4463fe8b289f678f4b5f
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:44:26 +0530

    Update PaymentHistory.js

--- src/components/parentPortal/PaymentHistory.js
+++ src/components/parentPortal/PaymentHistory.js
@@ -17,10 +17,12 @@ import {
 } from "react-bootstrap";
 import { format } from "date-fns";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const PaymentHistory = () => {
   const { studentId } = useParams();
