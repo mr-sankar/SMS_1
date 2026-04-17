ab195a14b50d79624417d76547c5588ec9181a25
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:48:02 +0530

    Update EditParent.js

--- src/components/parents/EditParent.js
+++ src/components/parents/EditParent.js
@@ -16,10 +16,12 @@ import {
   EyeOff,
 } from "lucide-react";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 // const API_URL = "http://localhost:5000/api";
 
