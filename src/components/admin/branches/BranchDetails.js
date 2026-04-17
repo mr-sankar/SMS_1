2f8a479dc1cd4af1b4b2c858ef2aa42cbcb72489
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:32:46 +0530

    Update BranchDetails.js

--- src/components/admin/branches/BranchDetails.js
+++ src/components/admin/branches/BranchDetails.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import "./BranchDetails.css";
-const BASE_URL = process.env.NODE_ENV === "production" 
-  ? process.env.REACT_APP_API_DEPLOYED_URL 
-  : process.env.REACT_APP_API_URL;
+// const BASE_URL = process.env.NODE_ENV === "production" 
+//   ? process.env.REACT_APP_API_DEPLOYED_URL 
+//   : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const BranchDetails = () => {
   const { branchId } = useParams();
