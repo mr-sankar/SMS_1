a9f696fee3415c852da46b624044e03c7eebf540
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:47:31 +0530

    Update feeDashboard.js

--- src/components/parentPortal/feeDashboard.js
+++ src/components/parentPortal/feeDashboard.js
@@ -15,10 +15,12 @@ import {
 } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const FeeDashboard = () => {
   const [student, setStudent] = useState(null);
