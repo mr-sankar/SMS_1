cc6ed8c9d81b2f44f576d1a486ef7f023f33177b
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:44:47 +0530

    Update PaymentStatus.js

--- src/components/parentPortal/PaymentStatus.js
+++ src/components/parentPortal/PaymentStatus.js
@@ -11,10 +11,12 @@ import {
 } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const PaymentStatus = () => {
   const location = useLocation();
