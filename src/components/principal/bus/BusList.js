e3d29d14e226c37a11f0742e2d83379cc9e61620
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:50:03 +0530

    Update BusList.js

--- src/components/principal/bus/BusList.js
+++ src/components/principal/bus/BusList.js
@@ -24,10 +24,13 @@ const BusList = () => {
 
   const driverFormRef = useRef(null);
 
-  const BASE_URL =
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_API_DEPLOYED_URL
-      : process.env.REACT_APP_API_URL;
+  // const BASE_URL =
+  //   process.env.NODE_ENV === "production"
+  //     ? process.env.REACT_APP_API_DEPLOYED_URL
+  //     : process.env.REACT_APP_API_URL;
+
+  const BASE_URL = process.env.REACT_APP_API_URL;
+  
   // const BASE_URL = 'http://localhost:5000';
 
   useEffect(() => {
