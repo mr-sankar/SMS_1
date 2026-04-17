fae5fc4d5df82f6b2f76cfc2319293e9d6b60afb
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 13:00:28 +0530

    Update healthRecord.js

--- src/components/students/healthRecord.js
+++ src/components/students/healthRecord.js
@@ -9,10 +9,12 @@ import HealthRecordManagement from './view';
 // const API_URL = 'http://localhost:5000/api';
 
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const getAuthConfig = () => {
   const token = localStorage.getItem('token');
