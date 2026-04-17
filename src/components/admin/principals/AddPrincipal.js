18d9446d30e1b12f3119ae57805296dfc701bab4
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:37:13 +0530

    Update AddPrincipal.js

--- src/components/admin/principals/AddPrincipal.js
+++ src/components/admin/principals/AddPrincipal.js
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 import "./AddPrincipal.css";
 
 // Define BASE_URL based on environment
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_DEPLOYED_URL
-    : process.env.REACT_APP_API_URL;
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_DEPLOYED_URL
+//     : process.env.REACT_APP_API_URL;
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const AddPrincipal = () => {
   const [formData, setFormData] = useState({
