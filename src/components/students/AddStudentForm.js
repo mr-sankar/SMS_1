dae6bb6b06e1153fc87dc0549c1faec4cc4fcd5a
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:58:58 +0530

    Update AddStudentForm.js

--- src/components/students/AddStudentForm.js
+++ src/components/students/AddStudentForm.js
@@ -459,10 +459,12 @@ const AddStudent = () => {
   };
   const sectionOptions = ["A", "B"];
 
-  const BASE_URL =
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_API_DEPLOYED_URL
-      : process.env.REACT_APP_API_URL;
+  // const BASE_URL =
+  //   process.env.NODE_ENV === "production"
+  //     ? process.env.REACT_APP_API_DEPLOYED_URL
+  //     : process.env.REACT_APP_API_URL;
+
+  const BASE_URL = process.env.REACT_APP_API_URL;
 
   const getAuthConfig = () => {
     const token = localStorage.getItem('token');
