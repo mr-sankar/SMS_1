84c604379ac59ead28bf754122eec2f92dd821a1
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-17 12:49:28 +0530

    Update ParentTable.js

--- src/components/parents/ParentTable.js
+++ src/components/parents/ParentTable.js
@@ -7,10 +7,12 @@ import {
   AlertCircle,
 } from "lucide-react";
 
-const BASE_URL = 
-process.env.NODE_ENV === "production"
-? process.env.REACT_APP_API_DEPLOYED_URL
-: process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// const BASE_URL = 
+// process.env.NODE_ENV === "production"
+// ? process.env.REACT_APP_API_DEPLOYED_URL
+// : process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 
 const ParentTable = ({ parents, onEdit, onDelete, onParentSelect }) => {
